a021ba9a7fa9d8b1bb6d3891ed6662f53e5e7fc3
Jakob Edelstein <jakobedelstein@gmail.com>
2022-03-29 08:34:01 -0600

    Add project files.

---
+++ ce_console.net_4.6.1/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+using JUST;
+using System.IO;
+
+namespace Test
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            string input = File.ReadAllText("/Users/Jake/source/repos/ce_console.net_4.6.1/ce_console.net_4.6.1/data/stair_configuration.json");
+
+            string transformer = File.ReadAllText("/Users/Jake/source/repos/ce_console.net_4.6.1/ce_console.net_4.6.1/data/landing_transformer.json"); ;
+
+
+            //JUSTContext context = new JUSTContext
+            //{
+            //    EvaluationMode = EvaluationMode.FallbackToDefault,
+
+            //};
+
+            string transformedString = new JsonTransformer().Transform(transformer, input);
+
+            Console.WriteLine(transformedString);
+        }
+    }
+}
