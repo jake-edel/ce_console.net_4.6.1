314ddb74e4319225174656f4a8ecfb4f35015593
Jakob Edelstein <jakobedelstein@gmail.com>
2022-03-30 10:10:30 -0600

    class for custom functions

---
+++ ce_console.net_4.6.1/CostHelper.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CostHelper
+{
+    public class StringerSolver
+    {
+        public static float test()
+        {
+            Console.WriteLine("test");
+            Console.ReadLine();
+
+            return 12.34F;
+        }
+    }
+}
