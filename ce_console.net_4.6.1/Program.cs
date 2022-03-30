e5e3f8365353ac6983078a5057c8018c8ec50fb4
Jakob Edelstein <jakobedelstein@gmail.com>
2022-03-30 10:10:02 -0600

    rename variables

--- ce_console.net_4.6.1/Program.cs
+++ ce_console.net_4.6.1/Program.cs
@@ -1,22 +1,37 @@
 ﻿using System;
 using JUST;
 using System.IO;
+using CostHelper;
+
 
 namespace CostEstimator
 {
+
     public class Program
     {
+        public static float Test()
+        {
+            Console.WriteLine("test");
+            Console.ReadLine();
+
+            return 12.34F;
+        }
         public static void Main(string[] args)
         {
             string input = File.ReadAllText("/Users/Jake/source/repos/ce_console.net_4.6.1/ce_console.net_4.6.1/data/stair_configuration.json");
 
-            string bom_transformer = File.ReadAllText("/Users/Jake/source/repos/ce_console.net_4.6.1/ce_console.net_4.6.1/data/landing_transformer.json"); ;
+            string towerTransformer = File.ReadAllText(@"/Users/Jake/source/repos/ce_console.net_4.6.1/ce_console.net_4.6.1/data/tower_transformer.json");
 
-            string transformedString = new JsonTransformer().Transform(bom_transformer, input);
+            string transformedTower = new JsonTransformer().Transform(towerTransformer, input);
 
-            Console.WriteLine(transformedString);
+            string bomTransformer = File.ReadAllText("/Users/Jake/source/repos/ce_console.net_4.6.1/ce_console.net_4.6.1/data/flight_transformer.json"); ;
+
+            string transformedString = new JsonTransformer().Transform(bomTransformer, input);
+
+            //Console.WriteLine(towerTransformer);
 
             File.WriteAllText("/Users/Jake/source/repos/ce_console.net_4.6.1/ce_console.net_4.6.1/output/output.json", transformedString);
+            File.WriteAllText("/Users/Jake/source/repos/ce_console.net_4.6.1/ce_console.net_4.6.1/output/full_tower_output.json", transformedTower);
         }
     }
 }
