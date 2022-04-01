5620cba3273ebf6e7a9b2cfe80e91c173c591ea2
Jakob Edelstein <jakobedelstein@gmail.com>
2022-04-01 07:40:51 -0600

    add comments

--- ce_console.net_4.6.1/Program.cs
+++ ce_console.net_4.6.1/Program.cs
@@ -18,18 +18,22 @@ namespace CostEstimator
                 DefaultDecimalPlaces = 3
             };
 
+            // Initial stair config file
             string config = File.ReadAllText("/Users/Jake/source/repos/ce_console.net_4.6.1/ce_console.net_4.6.1/data/stair_configuration.json");
 
+
+            // Combine initial config with material data and constants
             string materialsTransformer = File.ReadAllText(("/Users/Jake/source/repos/ce_console.net_4.6.1/ce_console.net_4.6.1/transformers/materials_transformer.json"));
             string transformedConfig = new JsonTransformer(context).Transform(materialsTransformer, config);
             File.WriteAllText("/Users/Jake/source/repos/ce_console.net_4.6.1/ce_console.net_4.6.1/output/config+material_output.json", transformedConfig);
 
 
+            // Iterate through flights and transform full tower
             //string towerTransformer = File.ReadAllText("/Users/Jake/source/repos/ce_console.net_4.6.1/ce_console.net_4.6.1/transformers/tower_transformer.json");
             //string transformedTower = new JsonTransformer(context).Transform(towerTransformer, config);
             //File.WriteAllText("/Users/Jake/source/repos/ce_console.net_4.6.1/ce_console.net_4.6.1/output/full_tower_output.json", transformedTower);
 
-
+            // Transform config/material JSON into data for a single flight
             string flightTransformer = File.ReadAllText("/Users/Jake/source/repos/ce_console.net_4.6.1/ce_console.net_4.6.1/transformers/flight_transformer.json"); ;
             string transformedFlight = new JsonTransformer(context).Transform(flightTransformer, transformedConfig);
             File.WriteAllText("/Users/Jake/source/repos/ce_console.net_4.6.1/ce_console.net_4.6.1/output/flight_output.json", transformedFlight);
