567f68f58fa20ed84c217d45a19fd79a5c3eaa0c
Jakob Edelstein <jakobedelstein@gmail.com>
2022-04-04 09:21:01 -0600

    add methods for calculating material lengths / quantities

--- ce_console.net_4.6.1/CostHelper.cs
+++ ce_console.net_4.6.1/CostHelper.cs
@@ -6,19 +6,37 @@ using System.Threading.Tasks;
 
 namespace CostMethods
 {
-    public class StringerSolver
+    public class FlightSolver
     {
-        public static double StringerArea(int rise, int run, int stringerWidth)
+        public static double StringerArea(int totalRise, int totalRun, int stringerWidth, bool isDdl, float rise, float run)
         {
-            double length = FindHypotenuse(rise, run);
+            double length = FindHypotenuse(totalRise, totalRun, isDdl, rise, run);
             double value = length * stringerWidth;
             return Math.Truncate(100 * value) / 100;
 
         }
-        public static double FindHypotenuse(int rise, int run)
+        public static double FindHypotenuse(float totalRise, float totalRun, bool isDdl, float rise, float run)
         {
-            double value = Math.Sqrt(Math.Pow(rise, 2) + Math.Pow(run, 2));
+            if (isDdl)
+            {
+                totalRise += rise;
+                totalRun += run;
+            }
+            double value = Math.Sqrt(Math.Pow(totalRise, 2) + Math.Pow(totalRun, 2));
             return Math.Truncate(100 * value) / 100;
+        }
+
+        public static int IntPostQuantity(int run)
+        {
+            float value = (float)run / 48;
+            return (int)Math.Ceiling(value);
+        }
+
+        public static int PicketQuantity(int run)
+        {
+            int value = run / 4;
+
+            return value;
         }
     }
 }
