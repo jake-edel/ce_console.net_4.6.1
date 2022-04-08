7b24b86201240bfdf347c0d71f5cdbfb8b55d05a
Jakob Edelstein <jakobedelstein@gmail.com>
2022-04-08 07:47:16 -0600

    class Material to reicieve material data

---
+++ ce_console.net_4.6.1/Material.cs
@@ -0,0 +1,13 @@
+﻿
+namespace CostEstimator
+{
+    public class Material
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string UnitOfMeasurement { get; set; }
+        public double LbsPerFoot { get; set; }
+        public double PricePerUnit { get; set; }
+
+    }
+}
