6f1795cb21c1192fbe539995bd091fb03801ab51
Jakob Edelstein <jakobedelstein@gmail.com>
2022-04-08 07:45:16 -0600

    remove context, begin deserializing json into memory

--- ce_console.net_4.6.1/Program.cs
+++ ce_console.net_4.6.1/Program.cs
@@ -1,6 +1,10 @@
 ﻿using System;
 using JUST;
 using System.IO;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+using System.Linq;
+using System.Collections.Generic;
 
 namespace CostEstimator
 {
@@ -8,37 +12,42 @@ namespace CostEstimator
     {
         public static void Main(string[] args)
         {
-            JUSTContext context = new JUSTContext
-            {
-                EvaluationMode = EvaluationMode.Strict,
-                DefaultDecimalPlaces = 3
-            };
 
             // Initial stair config file
-            string config = File.ReadAllText("/Users/Jake/source/repos/ce_console.net_4.6.1/ce_console.net_4.6.1/data/stair_configuration.json");
+            string config = File.ReadAllText("/Projects/ce_console.net_4.6.1/ce_console.net_4.6.1/data/stair_configuration.json");
 
 
-            // Combine initial config with material data and constants
-            string constantsTransformer = File.ReadAllText(("/Users/Jake/source/repos/ce_console.net_4.6.1/ce_console.net_4.6.1/transformers/constants_transformer.json"));
-            string transformedConfig = new JsonTransformer(context).Transform(constantsTransformer, config);
-            File.WriteAllText("/Users/Jake/source/repos/ce_console.net_4.6.1/ce_console.net_4.6.1/output/flightConsts.json", transformedConfig);
+            // Tranform each flight to create list of flight variables
+            string constantsTransformer = File.ReadAllText(("/Projects/ce_console.net_4.6.1/ce_console.net_4.6.1/transformers/constants_transformer.json"));
+            string transformedConfig = new JsonTransformer().Transform(constantsTransformer, config);
+            File.WriteAllText("/Projects/ce_console.net_4.6.1/ce_console.net_4.6.1/output/flight_consts.json", transformedConfig);
 
 
             // Iterate through flights and transform full tower
-            string towerTransformer = File.ReadAllText("/Users/Jake/source/repos/ce_console.net_4.6.1/ce_console.net_4.6.1/transformers/tower_transformer.json");
-            string transformedTower = new JsonTransformer(context).Transform(towerTransformer, transformedConfig);
-            File.WriteAllText("/Users/Jake/source/repos/ce_console.net_4.6.1/ce_console.net_4.6.1/output/full_tower_output.json", transformedTower);
-
-            string totalUnitsTransformer = File.ReadAllText("/Users/Jake/source/repos/ce_console.net_4.6.1/ce_console.net_4.6.1/transformers/weight_transformer.json");
-            string transformedWeight = new JsonTransformer(context).Transform(totalUnitsTransformer, transformedTower);
-            File.WriteAllText("/Users/Jake/source/repos/ce_console.net_4.6.1/ce_console.net_4.6.1/output/total_units_output.json", transformedWeight);
+            string towerTransformer = File.ReadAllText("/Projects/ce_console.net_4.6.1/ce_console.net_4.6.1/transformers/tower_transformer.json");
+            string transformedTower = new JsonTransformer().Transform(towerTransformer, transformedConfig);
+            File.WriteAllText("/Projects/ce_console.net_4.6.1/ce_console.net_4.6.1/output/full_tower_output.json", transformedTower);
 
 
 
-            // Transform config/material JSON into data for a single flight
-            string flightTransformer = File.ReadAllText("/Users/Jake/source/repos/ce_console.net_4.6.1/ce_console.net_4.6.1/transformers/flight_transformer.json"); ;
-            string transformedFlight = new JsonTransformer(context).Transform(flightTransformer, transformedConfig);
-            File.WriteAllText("/Users/Jake/source/repos/ce_console.net_4.6.1/ce_console.net_4.6.1/output/flight_output.json", transformedFlight);
+
+
+
+
+            string materialsJson = File.ReadAllText("/Projects/ce_console.net_4.6.1/ce_console.net_4.6.1/data/materials.json");
+
+
+
+            var materials = JsonConvert.DeserializeObject<Dictionary<string, Material>>(materialsJson);
+            Console.WriteLine(materials["0"].Name);
+            //Console.WriteLine(newList.MatList[0].Name);
+            //Console.WriteLine(newList.MatList[0].LbsPerFoot);
+            //Console.WriteLine(newList.MatList[1].Name);
+            //Console.WriteLine(newList.MatList[2].Name);
+            //Console.WriteLine(newList.MatList[3].Name);
+            //Console.WriteLine(newList.MatList[4].Name);
+            Console.ReadLine();
+
         }
     }
 }
