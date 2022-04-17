6e62b251f199a0aa8d19c26d425c30e68fdc946b
Jakob Edelstein <jakobedelstein@gmail.com>
2022-04-17 08:43:36 -0600

    iterate through flight parts and set each price

---
+++ ce_console.net_4.6.1/TowerCalculator.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections.Generic;
+
+namespace CostEstimator
+{
+    public class TowerCalculator
+    {
+        public Dictionary<string, Material> Materials { get; set; }
+        public Dictionary<string, List<Flight>> Flights { get; set; }
+
+        public TowerCalculator(Dictionary<string, List<Flight>> flights, Dictionary<string, Material> materials)
+        {
+            Materials = materials;
+            Flights = flights;
+
+        }
+
+        public void SetFlightPrices()
+        {
+            foreach (Flight flight in Flights["flights"])
+            {
+                foreach (Part part in flight.Stairs)
+                {
+                    part.SetPrice(Materials);
+                }
+
+                foreach (Part part in flight.Landing)
+                {
+                    part.SetPrice(Materials);
+                }
+
+                foreach (Part part in flight.Railing)
+                {
+                    part.SetPrice(Materials);
+
+                }
+            }
+        }
+    }
+}
